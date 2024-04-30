87d8e6a924ad6e6d5ff939b2ccc5ff32f6a0b06a
Elias Santistevan <elias.santistevan@sparkfun.com>
2024-04-30 14:26:19 -0600

    Adds function for enabling and setting timers

--- Firmware/main.c
+++ Firmware/main.c
@@ -6,26 +6,12 @@
 
 int main(void) {
 
-  ultrasonicCLKSetup();
-  // ultrasonicGPIOSetup();
-  ultrasonicI2CSetup();
+  setCLK();
+  // setGPIO();
+  setI2C();
+  setTimers();
 
-  // TIM2_DeInit();
-  // CLK_PeripheralClockConfig(CLK_Peripheral_TIM2, ENABLE);
-  // TIM2_TimeBaseInit(TIM2_Prescaler_8, TIM2_CounterMode_Up, 0xFFFF);
-  // TIM2_ARRPreloadConfig(ENABLE);
-
-  // TIM3_DeInit();
-  // CLK_PeripheralClockConfig(CLK_Peripheral_TIM3, ENABLE);
-  // TIM3_TimeBaseInit(TIM3_Prescaler_128, TIM3_CounterMode_Up, 5500);
-  // TIM3_ARRPreloadConfig(ENABLE);
-
-  // TIM4_DeInit();
-  // CLK_PeripheralClockConfig(CLK_Peripheral_TIM4, ENABLE);
-  // TIM4_TimeBaseInit(TIM4_Prescaler_128, TIM4_PERIOD);
-  // TIM4_ARRPreloadConfig(ENABLE);
-  //// FLASH_DeInit();
-
+  // FLASH_DeInit();
   // enableInterrupts();
 
   // TIM2_ClearFlag(TIM2_FLAG_Update);
@@ -36,15 +22,15 @@ int main(void) {
   // TIM3_ITConfig(TIM3_IT_Update, ENABLE);
   // TIM4_ITConfig(TIM4_IT_Update, ENABLE);
 
-  //enableOPAMP(0);
+  // enableOPAMP(0);
 
   while (1) {
     // Loop until something comes in.
-    if(COMMUNICATION_START == 1){
-      if(peripheralBuffer[0] == DISTANCE_READING){
+    if (COMMUNICATION_START == 1) {
+      if (peripheralBuffer[0] == DISTANCE_READING) {
         pulseTransmitter();
-        }
-      if(peripheralBuffer[0] ==  CHANGE_ADDRESS){
+      }
+      if (peripheralBuffer[0] == CHANGE_ADDRESS) {
         if (peripheralBuffer[1] != 0x00) {
           userAddress = peripheralBuffer[1];
           changeAddress(userAddress);
@@ -86,28 +72,28 @@ int main(void) {
       OUT_RANGE = 0;
       OPAMP_INT_TRIGGER = 0;
     }
-
   }
-  //while (1)
-  //  ;
+  // while (1)
+  //   ;
 }
 
 /*
- * @brief  delay function. 
+ * @brief  delay function.
  * @param  n : the time to delay
  * @retval None
-*/
+ */
 void delay(uint16_t n) {
   while (n--)
     ;
 }
 
 /*
- * @brief GPIO setup for the ultrasonic sensor: DIN1, DIN2, ECHO, TRIG, ADDR_RST.
+ * @brief GPIO setup for the ultrasonic sensor: DIN1, DIN2, ECHO, TRIG,
+ * ADDR_RST.
  * @param  None
  * @retval None
-*/
-void ultrasonicGPIOSetup(void) {
+ */
+void setGPIO(void) {
   GPIO_Init(GPIOD, (GPIO_Pin_TypeDef)GPIO_Pin_0,
             GPIO_Mode_Out_PP_Low_Fast); // DIN2
   GPIO_Init(GPIOB, (GPIO_Pin_TypeDef)GPIO_Pin_0,
@@ -133,8 +119,8 @@ void ultrasonicGPIOSetup(void) {
  * @brief I2C setup for the ultrasonic sensor.
  * @param  None
  * @retval None
-*/
-void ultrasonicI2CSetup(void) {
+ */
+void setI2C(void) {
   CLK_PeripheralClockConfig(CLK_Peripheral_I2C1, ENABLE);
   I2C_DeInit(I2C1);
   I2C_Init(I2C1, I2CSPEED, PERIPH_ADDRESS, I2C_Mode_I2C, I2C_DutyCycle_2,
@@ -148,8 +134,8 @@ void ultrasonicI2CSetup(void) {
  * @brief Default clock setup for the ultrasonic sensor.
  * @param  None
  * @retval None
-*/
-void ultrasonicCLKSetup(void) {
+ */
+void setCLK(void) {
   CLK_SYSCLKSourceConfig(CLK_SYSCLKSource_HSI);
   CLK_SYSCLKDivConfig(CLK_SYSCLKDiv_1);
   CLK_HSICmd(ENABLE);
@@ -158,10 +144,32 @@ void ultrasonicCLKSetup(void) {
 }
 
 /*
+ * @brief Enables and sets Timers two, three, and four.
+ * @param  None
+ * @retval None
+ */
+setTimers(void) {
+  TIM2_DeInit();
+  CLK_PeripheralClockConfig(CLK_Peripheral_TIM2, ENABLE);
+  TIM2_TimeBaseInit(TIM2_Prescaler_8, TIM2_CounterMode_Up, 0xFFFF);
+  TIM2_ARRPreloadConfig(ENABLE);
+
+  TIM3_DeInit();
+  CLK_PeripheralClockConfig(CLK_Peripheral_TIM3, ENABLE);
+  TIM3_TimeBaseInit(TIM3_Prescaler_128, TIM3_CounterMode_Up, 5500);
+  TIM3_ARRPreloadConfig(ENABLE);
+
+  TIM4_DeInit();
+  CLK_PeripheralClockConfig(CLK_Peripheral_TIM4, ENABLE);
+  TIM4_TimeBaseInit(TIM4_Prescaler_128, TIM4_PERIOD);
+  TIM4_ARRPreloadConfig(ENABLE);
+}
+
+/*
  * @brief Change the I2C address of the ultrasonic sensor.
  * @param  address: The new address to be set.
  * @retval None
-*/
+ */
 void changeAddress(uint8_t address) {
   I2C_DeInit(I2C1);
   I2C_Init(I2C1, I2CSPEED, address, I2C_Mode_I2C, I2C_DutyCycle_2,
@@ -172,10 +180,10 @@ void changeAddress(uint8_t address) {
 }
 
 /*
- * @brief Pulse the transmitter at 
+ * @brief Pulse the transmitter at
  * @param  None
  * @retval None
-*/
+ */
 void pulseTransmitter(void) {
 
   uint8_t i = 0;
@@ -215,7 +223,7 @@ void pulseTransmitter(void) {
  * @brief Enable or disable the op-amp.
  * @param  enable: 1 to enable, 0 to disable.
  * @retval None
-*/
+ */
 void enableOPAMP(uint8_t enable) {
   if (enable) {
     GPIO_Init(GPIOB, (GPIO_Pin_TypeDef)GPIO_Pin_4, GPIO_Mode_Out_PP_Low_Fast);
@@ -231,7 +239,7 @@ void enableOPAMP(uint8_t enable) {
  * @param  Addr: The address to write to.
  * @param  Data: The data to write.
  * @retval None
-*/
+ */
 void EEPROM_WriteByte(uint16_t Addr, uint8_t Data) {
 
   uint8_t *Ptr_SegAddr; // Segment pointer
@@ -262,7 +270,7 @@ void EEPROM_WriteByte(uint16_t Addr, uint8_t Data) {
  * @brief Read a byte from the EEPROM, used to read the I2C address.
  * @param  Addr: The address to read from.
  * @retval The data read.
-*/
+ */
 uint8_t EEPROM_ReadByte(uint16_t Addr) {
 
   uint16_t Data = 0;
